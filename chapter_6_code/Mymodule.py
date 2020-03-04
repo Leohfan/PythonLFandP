7558d469b877fab6780c39cfdc0891f3474c6be0
leohfan <leohfan@gmail.com>
2020-03-04 17:00:06 +0800

    textbook code

---
+++ chapter_6_code/Mymodule.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#一个名为 Mymodule.py 的模块
+def func():
+     print ('Mymodule.func()')
+class Myclass:
+     def myFunc(self):
+          print('Mymodule.Myclass.myFunc()')
+
