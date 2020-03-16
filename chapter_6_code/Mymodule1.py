eb88838fc4d856fc63ddee6d7ec5a903ae3f7fcc
leohfan <leohfan@gmail.com>
2020-03-16 21:20:11 +0800

    textbook code

---
+++ chapter_6_code/Mymodule1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+if __name__== '__main__':       #判断模块是否作为主程序运行
+    print('Mymodule 作为主程序运行！')
+else:
+    print('Mymodule 被另一个模块调用！')
+
+
+# In[ ]:
+
+
+
+
---
+++ chapter_6_code/call_Mymodule.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+'''This is the call_Mymodule modle. '''
+import Mymodule
+print __doc__        #调用模块属性__doc__
+
+
+# In[ ]:
+
+
+
+
